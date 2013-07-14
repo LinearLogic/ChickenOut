449a4baa330185ae9d2a79d90c13ce67f1dcbbaf
Jared Heath <silkrawnstudios@gmail.com>
2013-07-14 15:55:40 -0400

    Add cancellation (via config node) of egg laying events

--- src/com/entrocorp/linearlogic/chickenout/ChickenOut.java
+++ src/com/entrocorp/linearlogic/chickenout/ChickenOut.java
@@ -4,22 +4,30 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ChickenLayEggEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class ChickenOut extends JavaPlugin {
+public class ChickenOut extends JavaPlugin implements Listener {
 
 	private final String prefix = ChatColor.GRAY + "[" + ChatColor.GOLD + "Chicken" + ChatColor.YELLOW + ChatColor.ITALIC + "Out!" + ChatColor.GRAY + "] ";
 
+	@Override
 	public void onEnable() {
 		getLogger().info("Loading the config...");
 		saveDefaultConfig();
+		getLogger().info("Registering listener...");
+		getServer().getPluginManager().registerEvents(this, this);
 		getLogger().info("SQUAWK! Plugin successfully enabled.");
 	}
 
+	@Override
 	public void onDisable() {
 		saveConfig();
 	}
 
+	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (args.length == 0)
 			return sendMsg(sender, "Running version " + ChatColor.AQUA + getDescription().getVersion() + ChatColor.GRAY + " by LinearLogic.");
@@ -36,5 +44,11 @@ public class ChickenOut extends JavaPlugin {
 		sender.sendMessage(prefix + (msg.equals("no perms") ? "" + ChatColor.RED + ChatColor.ITALIC + "SQUAWK! " +
 				ChatColor.GRAY + "Access denied." : msg));
 		return true;
+	}
+
+	@EventHandler
+	public void onEggDrop(ChickenLayEggEvent event) {
+		if (getConfig().getBoolean("disable-egg-laying"))
+			event.setCancelled(true);
 	}
 }
